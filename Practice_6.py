6b4303a9780fbb79ca69741371ddee1b723858e4
Achew <artem.kovzik@yandex.ru>
2020-11-11 19:06:51 +0300

    Переделал практическое задание 6, теперь больше похоже на like Python

--- Practice_6.py
+++ Practice_6.py
@@ -18,18 +18,10 @@ while True:
 
 params = list(zip(*goods))[1]
 
-name_list = []
-price_list = []
-count_list = []
-unit_list = []
+a_goods = {key: [] for key in params[0].keys()}
 
 for el in params:
-    name_list.append(el.get('название'))
-    price_list.append(el.get('цена'))
-    count_list.append(el.get('количество'))
-    unit_list.append(el.get('единица измерения'))
-
-a_goods = {'название': name_list, 'цена': price_list, 'количество': count_list, 'единица_измерения': unit_list}
+    for key, value in el.items():
+        a_goods[key].append(value)
 
 print(a_goods)
-
